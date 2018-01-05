8a29d736165114b153e3bf375a2c2b5c1a85bb46
Malcolm E Barrett <malcolmbarrett@MacBook-Pro.local>
2018-01-05 09:50:47 -0800

    change known_confounder argument to no_shrink

--- R/modified_glmnet.R
+++ R/modified_glmnet.R
@@ -2,12 +2,12 @@
 
 #data <- simulate_data_set()
 
-cv_modified_elastic <- function(formula, data, x, known_confounders, alpha, ...) {
+cv_modified_elastic <- function(formula, data, x, no_shrink, alpha, ...) {
   #  add option to not use formula, per usual?
   #  add option to be more flexible with what isn't shrunk
   independent_vars <- model.matrix(formula, data = data)[, -1]
   vars <- colnames(independent_vars)
-  
+
   y_name <- as.character(formula)[2]
   dependent_vars <- data[[y_name]]
   # if (formula[2] == ".") {
@@ -17,26 +17,26 @@ cv_modified_elastic <- function(formula, data, x, known_confounders, alpha, ...)
   #   vars <- formula[[2]] %>% stringr::str_split(" \\+ ") %>% purrr::pluck(1)
   #   independent_vars <- as.matrix(data[, vars])
   # }
-  
+
   penalty.factor <- c(0, rep(1, length(vars) - 1))
   .cv_model <- glmnet::cv.glmnet(y = dependent_vars, x = independent_vars, alpha = alpha, penalty.factor = penalty.factor, ...)
-  .model <- .cv_model$glmnet.fit %>% broom::tidy() %>% filter(lambda == .cv_model$lambda.min) 
-  
+  .model <- .cv_model$glmnet.fit %>% broom::tidy() %>% filter(lambda == .cv_model$lambda.min)
+
   if (exponentiate) {
     .model <- .model %>% mutate(estimate = exp(estimate))
   }
-  
+
   cfdr_glmnet <- list(model = .cv_model, results = .model)
   class(cfdr_glmnet) <- "cfdr_glmnet"
   cfdr_glmnet
 }
 
-cfdr_cv_lasso <- function(data, formula, x = NULL, known_confounders = NULL, exponentiate = FALSE, ...) {
-  cv_modified_elastic(formula, data, x, known_confounders, alpha = 1, ...)
+cfdr_cv_lasso <- function(data, formula, x = NULL, no_shrink = NULL, exponentiate = FALSE, ...) {
+  cv_modified_elastic(formula, data, x, no_shrink, alpha = 1, ...)
 }
 
-cv_modified_ridge <- function(formula, data, x, known_confounders, ...) {
-  cv_modified_elastic(formula, data, x, known_confounders, alpha = 0, ...)
+cv_modified_ridge <- function(formula, data, x, no_shrink, ...) {
+  cv_modified_elastic(formula, data, x, no_shrink, alpha = 0, ...)
 }
 
 print.cfdr_glmnet <- function(.cfdr_glmnet) {
